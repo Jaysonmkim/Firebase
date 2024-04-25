6ed2cb3d31fe09b7ab2b2a7e21e43fe6d970fd1c
Jaysonmkim <kimuhujayson@gmail.com>
2024-04-25 13:04:11 +0300

    Initial commit

---
+++ app/src/main/java/com/jayson/firebase/navigation/AppNavHost.kt
@@ -0,0 +1,47 @@
+package com.jayson.firebase.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Modifier.Companion
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.jayson.firebase.ui.theme.screens.home.HomeScreen
+import com.jayson.firebase.ui.theme.screens.login.LoginScreen
+import com.jayson.firebase.ui.theme.screens.products.AddProductsScreen
+import com.jayson.firebase.ui.theme.screens.products.UpdateProductsScreen
+import com.jayson.firebase.ui.theme.screens.products.ViewProductsScreen
+import com.jayson.firebase.ui.theme.screens.products.ViewUploadsScreen
+import com.jayson.firebase.ui.theme.screens.register.RegisterScreen
+
+
+@Composable
+fun AppNavHost(modifier: Modifier =Modifier, navController:NavHostController= rememberNavController(), startDestination:String= ROUTE_LOGIN) {
+
+    NavHost(navController = navController, modifier=Modifier, startDestination = startDestination ){
+        composable(ROUTE_LOGIN){
+            LoginScreen(navController)
+        }
+        composable(ROUTE_REGISTER){
+            RegisterScreen(navController)
+        }
+
+        composable(ROUTE_HOME){
+            HomeScreen(navController)
+        }
+        composable(ROUTE_ADD_PRODUCT) {
+            AddProductsScreen(navController)
+        }
+        composable(ROUTE_VIEW_PRODUCT){
+            ViewProductsScreen(navController)
+        }
+        composable(ROUTE_UPDATE_PRODUCT+ "/{id}"){passedData ->
+            UpdateProductsScreen(navController,passedData.arguments?.getString("id")!!)
+        }
+        composable(ROUTE_VIEW_UPLOAD){
+            ViewUploadsScreen(navController)
+        }
+    }
+
+}
